5bb504af23f9d1033ad8911057a8ff7446ccb96f
unknown <a.okovalev@yandex.ru>
2021-05-10 20:42:43 +0300

    Refactor: index.js was created in routes. All routes from app.js were moved to index.js.

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const { errors } = require('celebrate');
-const { celebrate, Joi } = require('celebrate');
 const helmet = require('helmet');
 require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
 const bodyParser = require('body-parser');
-const router = require('express').Router();
 const mongoose = require('mongoose');
 const cors = require('cors');
-
-const { login, createUser, logout } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const NotFoundError = require('./errors/not-found-err');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./helpers/limiter');
+const routes = require('./routes');
 
 const app = express();
 const { MONGODB_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
@@ -30,53 +25,15 @@ app.use(cors());
 app.use(helmet());
 app.disable('x-powered-by');
 
-/** При получение данных используй эту функцию для обработки данных */
+// /** При получение данных используй эту функцию для обработки данных */
 app.use(bodyParser.json());
 
 app.use(requestLogger); // подключаем логгер запросов
 
-const userRouter = require('./routes/users');
-const movieRouter = require('./routes/movies');
-
 // apply to all requests
 app.use(limiter);
 
-// роуты, не требующие авторизации,
-app.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login,
-);
-
-app.post(
-  '/signout',
-  logout,
-);
-
-app.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
-    }),
-  }),
-  createUser,
-);
-
-app.use(router);
-router.use('/users', auth, userRouter);
-router.use('/movies', auth, movieRouter);
-
-router.use('*', (req, res, next) => {
-  next(new NotFoundError('Страница не найдена'));
-});
+app.use(routes);
 
 app.use(errorLogger); // подключаем логгер ошибок
 
